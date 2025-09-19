3c76b27ac1bf54d01af070682acf8ae92182e153
Jan <janp1krc@gmail.com>
2025-09-19 12:28:25 +0200

    Paths to truck site

--- truck_company/trucks/urls.py
+++ truck_company/trucks/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = "trucks"
 urlpatterns = [
-        # path("", views.index, name="index"),
+        path("", views.index, name="index"),
         path("<int:truck_id>/", views.display_truck_info, name="display_truck_info"),
         ]
